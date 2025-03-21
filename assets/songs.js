c133807b73577460e66282d35cbfe8987eea06c3
kristir210 <kristervestland@hotmail.no>
2025-03-21 13:49:28 +0100

    Bunch of changes

--- assets/songs.js
+++ assets/songs.js
@@ -1,7 +1,323 @@
+// songs.js
 export const songs = [
-    { name: "Bohemian Rhapsody", artist: "Queen" },
-    { name: "Stairway to Heaven", artist: "Led Zeppelin" },
-    { name: "Hotel California", artist: "Eagles" },
-    { name: "Imagine", artist: "John Lennon" },
-    { name: "Wonderwall", artist: "Oasis" }
-  ];
+  {
+    name: "Kickstart My Heart",
+    artist: "Mötley Crüe"
+  },
+  {
+    name: "Crazy Train",
+    artist: "Ozzy Osbourne"
+  },
+  {
+    name: "For Whom The Bell Tolls",
+    artist: "Metallica"
+  },
+  {
+    name: "Iron Man",
+    artist: "Black Sabbath"
+  },
+  {
+    name: "Shout At The Devil",
+    artist: "Mötley Crüe"
+  },
+  {
+    name: "T.N.T.",
+    artist: "AC/DC"
+  },
+  {
+    name: "Hot for Teacher",
+    artist: "Van Halen"
+  },
+  {
+    name: "Live Wire",
+    artist: "Mötley Crüe"
+  },
+  {
+    name: "Thunderstruck",
+    artist: "AC/DC"
+  },
+  {
+    name: "Panama",
+    artist: "Van Halen"
+  },
+  {
+    name: "Take Five",
+    artist: "The Dave Brubeck Quartet"
+  },
+  {
+    name: "So What (feat. John Coltrane, Cannonball Adderley & Bill Evans)",
+    artist: "Miles Davis"
+  },
+  {
+    name: "My Little Brown Book",
+    artist: "Duke Ellington & John Coltrane"
+  },
+  {
+    name: "My Funny Valentine",
+    artist: "Miles Davis Quintet"
+  },
+  {
+    name: "Little Lies - 2017 Remaster",
+    artist: "Fleetwood Mac"
+  },
+  {
+    name: "Gimme! Gimme! Gimme! (A Man After Midnight)",
+    artist: "ABBA"
+  },
+  {
+    name: "Assumptions",
+    artist: "Sam Gellaitry"
+  },
+  {
+    name: "Yours Truly, Austin Post",
+    artist: "Post Malone"
+  },
+  {
+    name: "Bad Moon Rising",
+    artist: "Creedence Clearwater Revival"
+  },
+  {
+    name: "Sunshine Of Your Love",
+    artist: "Cream"
+  },
+  {
+    name: "Layla",
+    artist: "Derek & The Dominos"
+  },
+  {
+    name: "Purple Haze",
+    artist: "Jimi Hendrix"
+  },
+  {
+    name: "All Along the Watchtower",
+    artist: "Jimi Hendrix"
+  },
+  {
+    name: "Born To Be Wild",
+    artist: "Steppenwolf"
+  },
+  {
+    name: "How You Remind Me",
+    artist: "Nickelback"
+  },
+  {
+    name: "Levitating",
+    artist: "Dua Lipa"
+  },
+  {
+    name: "Don't Start Now",
+    artist: "Dua Lipa"
+  },
+  {
+    name: "I Walk the Line",
+    artist: "Johnny Cash"
+  },
+  {
+    name: "Ring of Fire",
+    artist: "Johnny Cash"
+  },
+  {
+    name: "Cocaine",
+    artist: "Eric Clapton"
+  },
+  {
+    name: "Sweet Home Alabama",
+    artist: "Lynyrd Skynyrd"
+  },
+  {
+    name: "Heart-Shaped Box",
+    artist: "Nirvana"
+  },
+  {
+    name: "Lithium",
+    artist: "Nirvana"
+  },
+  {
+    name: "Breed",
+    artist: "Nirvana"
+  },
+  {
+    name: "About A Girl",
+    artist: "Nirvana"
+  },
+  {
+    name: "Rock and Roll - Remaster",
+    artist: "Led Zeppelin"
+  },
+  {
+    name: "Misty Mountain Hop - Remaster",
+    artist: "Led Zeppelin"
+  },
+  {
+    name: "Whole Lotta Love - Remaster",
+    artist: "Led Zeppelin"
+  },
+  {
+    name: "Breaking the Law",
+    artist: "Judas Priest"
+  },
+  {
+    name: "Fleur de Lis",
+    artist: "Duke Ellington & John Coltrane"
+  },
+  {
+    name: "It Never Ends",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Can't Stop",
+    artist: "Red Hot Chili Peppers"
+  },
+  {
+    name: "Houdini",
+    artist: "Dua Lipa"
+  },
+  {
+    name: "Pon de Replay",
+    artist: "Rihanna"
+  },
+  {
+    name: "Immigrant Song - Remaster",
+    artist: "Led Zeppelin"
+  },
+  {
+    name: "Ace of Spades",
+    artist: "Motörhead"
+  },
+  {
+    name: "Killing Me Slowly",
+    artist: "Bad Wolves"
+  },
+  {
+    name: "Zombie",
+    artist: "Bad Wolves"
+  },
+  {
+    name: "Run For Your Life",
+    artist: "Bad Wolves"
+  },
+  {
+    name: "Learn To Walk Again",
+    artist: "Bad Wolves"
+  },
+  {
+    name: "Sugar Honey Ice & Tea",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Atlas, Rise!",
+    artist: "Metallica"
+  },
+  {
+    name: "Moth Into Flame",
+    artist: "Metallica"
+  },
+  {
+    name: "Girls, Girls, Girls",
+    artist: "Mötley Crüe"
+  },
+  {
+    name: "The House of Wolves",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Empire (Let Them Sing)",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Crooked Young",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Antivist",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Happy Song",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Throne",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Teardrops",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "Dear Diary,",
+    artist: "Bring Me The Horizon"
+  },
+  {
+    name: "The Way You Used To Do",
+    artist: "Queens of the Stone Age"
+  },
+  {
+    name: "Shot In The Dark",
+    artist: "AC/DC"
+  },
+  {
+    name: "Seek & Destroy",
+    artist: "Metallica"
+  },
+  {
+    name: "Looks That Kill",
+    artist: "Mötley Crüe"
+  },
+  {
+    name: "I Don't Want To Set The World On Fire",
+    artist: "The Ink Spots"
+  },
+  {
+    name: "Orange Colored Sky",
+    artist: "Nat King Cole"
+  },
+  {
+    name: "Big Iron",
+    artist: "Marty Robbins"
+  },
+  {
+    name: "Blue Moon - 1999 Remastered",
+    artist: "Frank Sinatra"
+  },
+  {
+    name: "A Wonderful Guy",
+    artist: "Tex Beneke"
+  },
+  {
+    name: "Take Me Home, Country Roads",
+    artist: "John Denver"
+  },
+  {
+    name: "Ain't That A Kick In The Head",
+    artist: "Dean Martin"
+  },
+  {
+    name: "The Wanderer",
+    artist: "Dion"
+  },
+  {
+    name: "The End of the World",
+    artist: "Skeeter Davis"
+  },
+  {
+    name: "Uranium Rock",
+    artist: "Warren Smith"
+  },
+  {
+    name: "Heartaches By The Numbers",
+    artist: "Guy Mitchell"
+  },
+  {
+    name: "Jingle Jangle Jingle",
+    artist: "Kay Kyser & His Orchestra"
+  },
+  {
+    name: "Sixteen Tons",
+    artist: "Tennessee Ernie Ford"
+  },
+  {
+    name: "Come Fly With Me",
+    artist: "Frank Sinatra"
+  }
+];
